8ffc5ce7fb87bc70487cdef5257d261364593557
Booda B <boodab@Boodas-MacBook-Pro.local>
2025-10-11 21:09:08 -0400

    Fix: PlanetScale SSL connection in dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,18 +1,27 @@
 <?php
-$servername = getenv('DB_HOST');
-$username   = getenv('DB_USERNAME');
-$password   = getenv('DB_PASSWORD');
-$dbname     = getenv('DB_DATABASE');
+    $servername = getenv('DB_HOST');
+    $username   = getenv('DB_USERNAME');
+    $password   = getenv('DB_PASSWORD');
+    $dbname     = getenv('DB_DATABASE');
+    $ssl_ca     = getenv('DB_SSL_CA');
 
-$ssl_ca = getenv('DB_SSL_CA');
+    $conn = mysqli_init();
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
-mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+    mysqli_real_connect(
+        $conn,
+        $servername,
+        $username,
+        $password,
+        $dbname,
+        3306,
+        NULL,
+        MYSQLI_CLIENT_SSL,
+        $ssl_ca
+    );
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-date_default_timezone_set('America/New_York');
-error_reporting(0);
+    date_default_timezone_set('America/New_York');
+    error_reporting(0);
