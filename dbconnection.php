f3692fa07bf7be8ef4ab3de0a64b15a9cb6e4330
Booda B <boodab@Boodas-MacBook-Pro.local>
2025-10-11 21:34:22 -0400

    Enable error reporting in dbconnection.php for debugging

--- dbconnection.php
+++ dbconnection.php
@@ -15,4 +15,7 @@
     }
 
     date_default_timezone_set('America/New_York');
-    error_reporting(0);
+    // error_reporting(0);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
