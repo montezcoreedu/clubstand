0d869645861f7a9de56a2c08c201abca22901fbf
Booda B <boodab@Boodas-MacBook-Pro.local>
2025-10-11 21:02:20 -0400

    Fix: Added PlanetScale SSL connection to dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,14 +1,18 @@
 <?php
-    $servername = getenv('DB_HOST');
-    $username   = getenv('DB_USERNAME');
-    $password   = getenv('DB_PASSWORD');
-    $dbname     = getenv('DB_NAME');
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USERNAME');
+$password   = getenv('DB_PASSWORD');
+$dbname     = getenv('DB_DATABASE');
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+$ssl_ca = getenv('DB_SSL_CA');
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
-    date_default_timezone_set('America/New_York');
-    error_reporting(0);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+date_default_timezone_set('America/New_York');
+error_reporting(0);
